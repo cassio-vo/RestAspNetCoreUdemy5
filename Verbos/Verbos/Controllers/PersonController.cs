5493265a7c500c1f3703131e053c470b74a79948
Cassio <cassio_de_vargas@yahoo.com.br>
2021-03-23 23:39:22 -0300

    person

---
+++ Verbos/Verbos/Controllers/PersonController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Calculadora.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{first}/{second}")]
+        public IActionResult Get(string first, string second)
+        {
+            if (IsNumeric(first) && IsNumeric(second))
+            {
+                var sum = ConvertToDceimal(first) + ConvertToDceimal(second);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDceimal(string strNumber)
+        {
+            decimal number;
+            if (decimal.TryParse(strNumber, out number))
+                return number;
+
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
