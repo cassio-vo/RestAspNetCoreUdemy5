28600b835a1a4b162444baa2f4cd348cc9fbd468
Cassio de Vargas Oliveira <cassio_de_vargas@yahoo.com.br>
2021-06-20 16:32:44 -0300

    Criação da estrutura inicial de banco de dados

---
+++ Verbos/Verbos/Model/Context/MySqlContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Verbos.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext()
+        { }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options): base(options)
+        {}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
