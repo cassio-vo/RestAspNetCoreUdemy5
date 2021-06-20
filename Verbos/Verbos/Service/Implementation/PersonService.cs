9dd4083591ccfc9a883ebd4e5510bd4fb851071d
Cassio de Vargas Oliveira <cassio_de_vargas@yahoo.com.br>
2021-06-20 17:45:39 -0300

    crud terminado

--- Verbos/Verbos/Service/Implementation/PersonService.cs
+++ Verbos/Verbos/Service/Implementation/PersonService.cs
@@ -18,12 +18,49 @@ namespace Verbos.Service.Implementation
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id))
+                return new Person();
+
+            var result = FindById(person.Id);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
             return person;
         }
 
         public void Delete(long id)
         {
-            throw new NotImplementedException();
+            var result = FindById(id);
+            if (result != null)
+            {
+                _context.Persons.Remove(result);
+                _context.SaveChanges();
+            }
         }
 
         public List<Person> FindAll()
@@ -33,12 +70,12 @@ namespace Verbos.Service.Implementation
 
         public Person FindById(long id)
         {
-            throw new NotImplementedException();
+            return _context.Persons.Single(x => x.Id == id); 
         }
 
-        public Person Update(Person person)
+        private bool Exists(long id)
         {
-            throw new NotImplementedException();
+            return _context.Persons.Any(x => x.Id == id);
         }
     }
 }
